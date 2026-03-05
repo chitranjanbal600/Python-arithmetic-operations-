16f5efe48b3f99fec6984b7aea342f4c32291a0e
Chitranjan <chitranjanbal600@gmail.com>
2026-03-05 11:47:12 +0530

    Add basic arithmetic operations in Python

---
+++ Arithmetic_operations.py
@@ -0,0 +1,32 @@
+# Arithmetic Operations in Python
+
+num1 = 12
+num2 = 12
+
+# Addition
+sum_result = num1 + num2
+print("Sum:", sum_result)
+
+# Subtraction
+sub = num1 - num2
+print("Subtraction:", sub)
+
+# Multiplication
+mul = num1 * num2
+print("Multiplication:", mul)
+
+# Division
+div = num1 / num2
+print("Division:", div)
+
+# Floor Division
+floor_division = num1 // num2
+print("Floor Division:", floor_division)
+
+# Modulus
+mod = num1 % num2
+print("Modulus:", mod)
+
+# Exponent
+exp = num1 ** num2
+print("Exponent:", exp)
